716768a739caec0010efbd5b8700838ec4f6277c
Abru69 <118375491+Abru69@users.noreply.github.com>
2024-05-26 18:12:51 -0600

    Add files via upload

---
+++ CasiFullPhp/Menus/MenoPizza.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Little Ceasars</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="../css/styles.css" rel="stylesheet">
+</head>
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="../index.php">Delivery App</a>
+    
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="Menu.php">Menu</a>
+          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Acerca de nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contactos</a>
+            </li>
+          </ul>
+    </div>
+    </nav>
+
+
+    <br>
+    <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col">
+            <div class="card" style="width: 16rem;">
+                <img src="../IMG/Pizza 2.jpg" class="card-img-top" width="100">
+                <div class="card-body">
+                  <h5 class="card-title">Pizza de queso</h5>
+                  <p class="card-text">Cualquier ingrediente extra se cobrara extra</p>
+                  <a href="../CarritoRestaurant.html" class="btn btn-primary">Entrar</a>
+                    </div>
+                </div>
+            </div>
+          <div class="col">
+            <div class="card" style="width: 16rem;">
+                <img src="../IMG/Pizza 1.jpg" class="card-img-top" width="100">
+                <div class="card-body">
+                  <h5 class="card-title">Pizza peperoni</h5>
+                  <p class="card-text">Cualquier ingrediente extra se cobrara extra</p>
+                  <a href="../CarritoRestaurant.html" class="btn btn-primary">Entrar</a>
+                </div>
+              </div>
+            </div>
+          <div class="col">
+            <div class="card" style="width: 16rem;">
+                <img src="../IMG/Pizza 3.jpg" class="card-img-top" width="100">
+                <div class="card-body">
+                  <h5 class="card-title">Pizza chorizo</h5>
+                  <p class="card-text">Cualquier ingrediente extra se cobrara extra</p>
+                  <a href="../CarritoRestaurant.html" class="btn btn-primary">Entrar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+
+      <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col">
+            <div class="card" style="width: 16rem;">
+                <img src="../IMG/Coca.jpg" class="card-img-top" width="100">
+                <div class="card-body">
+                  <h5 class="card-title">Coca Cola</h5>
+                  <a href="../CarritoRestaurant.html" class="btn btn-primary">Entrar</a>
+                    </div>
+                </div>
+            </div>
+          <div class="col">
+            <div class="card" style="width: 16rem;">
+                <img src="../IMG/Sprite.jpg" class="card-img-top" width="100">
+                <div class="card-body">
+                  <h5 class="card-title">Sprite</h5>
+                  <a href="../CarritoRestaurant.html" class="btn btn-primary">Entrar</a>
+                </div>
+              </div>
+            </div>
+          <div class="col">
+            <div class="card" style="width: 16rem;">
+                <img src="../IMG/Fanta.jpg" class="card-img-top" width="100">
+                <div class="card-body">
+                  <h5 class="card-title">Fanta</h5>
+                  <a href="../CarritoRestaurant.html" class="btn btn-primary">Entrar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php require("../Pie.php"); ?>
+
+
+
+
+
+
+    <!-- Bootstrap JS -->
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
